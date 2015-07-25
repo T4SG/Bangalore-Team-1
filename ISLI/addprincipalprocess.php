fd1e5cb2940af8aba7622c5524ef22334afa87cc
parth9451 <parth_9451@yahoo.com>
2015-07-25 19:32:04 +0530

    addprinci

--- ISLI/addprincipalprocess.php
+++ ISLI/addprincipalprocess.php
@@ -1,13 +1,14 @@
 <?php
-include('connect.php');
+include('connect.php');  
 // Get values from form 
-$tbl_name="principal"; // Table name 
+$tbl_name="principal_register"; // Table name 
 // Get values from form 
-$principal_id=$_POST['lg_id'];
-$username=$_POST['header'];
-$password=$_POST['category'];
+$principal_name=$_POST['name'];
+$username=$_POST['username'];
+$password=$_POST['password'];
+$email_id=$_POST['email'];
 // Insert data into mysql 
-$sql="INSERT INTO $tbl_name(principal_id,username,password)VALUES('$principal_id','$username','$password')";
+$sql="INSERT INTO $tbl_name(principal_id,principal_name,username,password,email_id)VALUES('$principal_id','$principal_name','$username','$password','$email_id')";
 $result=mysql_query($sql);
 // if successfully insert data into database, displays message "Successful". 
 if($result){
