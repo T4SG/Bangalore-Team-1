ae8740424e6b3141d5a2187d57d893f1110d8609
shashankgurudu <shashank233@gmail.com>
2015-07-25 19:45:10 +0530

    login with database principal

---
+++ ISLI/s-loginverify.php
@@ -0,0 +1,34 @@
+<?php
+session_start(); // Starting Session
+if (isset($_POST['actionlogin'])) {
+if (empty($_POST['username']) || empty($_POST['password'])) {
+$error = "Username or Password is invalid";
+echo "$error";
+}
+else
+{
+$username=$_POST['username'];
+$password=$_POST['password'];
+include("connect.php");
+// To protect MySQL injection for Security purpose
+$username = stripslashes($username);
+$password = stripslashes($password);
+$username = mysql_real_escape_string($username);
+$password = mysql_real_escape_string($password);
+// Selecting Database
+$db = mysql_select_db("ISLI", $connection);
+// SQL query to fetch information of registerd users and finds user match.
+$query = mysql_query("select * from isli_staff where staff_id='$username' AND password='$password'", $connection);
+$rows = mysql_num_rows($query);
+echo "$rows";
+if ($rows == 1) {
+$_SESSION['login_user']=$username; // Initializing Session
+header("location: petition.php"); // Redirecting To Other Page
+} else {
+$error = "Username or Password is invalid";	
+echo "$error";
+}
+mysql_close($connection); // Closing Connection
+}
+}
+?>
