5e79a889aa86ce57a907ff7048ea31a5b02473b4
parth9451 <parth_9451@yahoo.com>
2015-07-26 07:14:09 +0530

    2

---
+++ ISLI/domain2.php
@@ -0,0 +1,21 @@
+<?php
+include('connect.php');
+// Get values from form 
+$tbl_name="priority"; // Table name 
+// Get values from form 
+$sql="SELECT * FROM $tbl_name WHERE domain = '2' ";
+$result=mysql_query($sql);
+if (mysql_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysql_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " priorityname: " . $row["priority_name"]. " status" . $row["status"]." rank: " . $row["rank"]. " domain: " . $row["domain"]. " score". $row["score"]."<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+?>
+<?php 
+// close connection 
+mysql_close();
+?>
